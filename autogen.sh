7b22cce1afd11f0005af042f70a4cd5ccdab7c40
root <root@tvbox22.(none)>
2012-08-04 11:08:00 -0700

    ii

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 echo >NEWS
 echo >AUTHORS
 echo >ChangeLog
-(aclocal && autoheader && libtoolize && automake --add-missing && autoconf) || exit 1
+(autoheader && aclocal -I m4 --install && libtoolize && autoconf && automake --add-missing) || exit 1
 mkdir -p m4 || exit 1
 # ./configure --prefix=/usr || exit 1
 
