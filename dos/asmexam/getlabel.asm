62d0b22f73f2c7549bcd392c40f1247e322f2de4
root <root@tvbox22.(none)>
2012-09-27 05:51:14 -0700

    iiii

---
+++ dos/asmexam/getlabel.asm
@@ -0,0 +1,81 @@
+;--------------------------------------------------------------------------------------
+; GETLABEL.COM
+;
+; Read the label from the current disk 
+;--------------------------------------------------------------------------------------
+		bits 16			; 16-bit real mode
+		org 0x100		; DOS .COM executable starts at 0x100 in memory
+
+		segment .text
+
+		push	cs
+		pop	ds
+		push	cs
+		pop	es
+
+; zero memory in FCB
+		cld
+		mov	di,fcb
+		xor	ax,ax
+		mov	cx,0x2A/2
+		rep	stosw
+
+; make extended FCB
+		mov	byte [fcb],0xFF
+		mov	byte [fcb+6],0x08; match volume label
+		mov	di,fcb+8
+		mov	cx,8+3
+		mov	al,'?'
+		cld
+		rep	stosb
+
+; info is returned in the DTA, so set DTA location
+		mov	ah,0x1A		; AH=0x1A set DTA
+		mov	dx,dta
+		int	21h
+
+; carry out the enumeration
+		mov	ah,0x11		; AH=0x11 find first file
+		mov	dx,fcb
+		int	21h
+		cmp	al,0x00
+		jnz	done
+
+; print what was returned
+print_info:	cld
+		mov	cx,11		; print name
+		mov	si,dta+1+7
+		call	putslen
+
+		mov	ah,0x09
+		mov	dx,crlf
+		int	21h
+
+done:		xor	ah,ah
+		int	21h
+
+; entry:
+;   CX = number of chars
+;   SI = string
+putslen:	push	cx
+putslenloop1:	lodsb
+		cmp	al,' '
+		jz	putslenloop2
+		mov	dl,al
+		mov	ah,0x02
+		int	21h
+putslenloop2:	loop	putslenloop1
+		pop	cx
+		ret
+
+		segment .data
+
+crlf:		db	13,10,'$'
+
+		segment .bss
+
+fcb:		resb	0x2A
+
+; WARNING: We don't know how large DOS will make the record size, therefore this must be last!
+dta:		resb	0x100
+
---
+++ dos/asmexam/setlabel.asm
@@ -0,0 +1,117 @@
+;--------------------------------------------------------------------------------------
+; GETLABEL.COM
+;
+; Read the label from the current disk 
+;--------------------------------------------------------------------------------------
+		bits 16			; 16-bit real mode
+		org 0x100		; DOS .COM executable starts at 0x100 in memory
+
+		segment .text
+
+		push	cs
+		pop	ds
+		push	cs
+		pop	es
+
+; zero memory in FCB
+		cld
+		mov	di,fcb
+		xor	ax,ax
+		mov	cx,(0x2A+0x2A)/2
+		rep	stosw
+
+; make extended FCB
+		cld
+		mov	al,' '
+		mov	byte [fcb],0xFF
+		mov	byte [fcb+6],0x08; will be volume label
+		mov	di,fcb+7+1
+		mov	cx,8+3
+		rep	stosb
+
+		mov	byte [fcbren],0xFF
+		mov	byte [fcbren+6],0x08; match volume label
+		mov	di,fcbren+7+1
+		mov	cx,8+3
+		mov	al,'?'
+		rep	stosb
+
+; copy the new label from the command line
+		mov	di,fcb+8
+		mov	si,0x81
+l1:		lodsb
+		cmp	al,' '
+		jz	l1
+		cmp	al,0x0D
+		jz	l1e
+		stosb
+		mov	cx,8+3-1
+l1main:		lodsb
+		cmp	al,' '
+		jz	l1e
+		cmp	al,0x0D
+		jz	l1e
+		stosb
+		loop	l1main
+l1e:
+
+; make sure the create FCB's name is the same as the rename FCB's target
+		mov	si,fcb+8
+		mov	di,fcbren+7+0x11
+		mov	cx,8+3
+		rep	movsb
+
+; if the name is empty, then delete the label
+		cmp	byte [fcb+8],' '
+		jnz	not_delete
+;------------DELETION-----------
+		mov	ah,0x13
+		mov	dx,fcb
+		int	21h
+		mov	dx,str_deleted
+		cmp	al,0x00
+		jz	delete_ok
+		mov	dx,str_del_fail
+delete_ok:	jmp	short common_str_err
+
+; start by trying to rename the volume label
+not_delete:	mov	ah,0x17
+		mov	dx,fcbren
+		int	21h
+		mov	dx,str_renamed
+		cmp	al,0x00
+		jz	short common_str_err
+
+; OK, then try to create
+not_rename:	mov	ah,0x16
+		mov	dx,fcb
+		int	21h
+		mov	dx,str_created
+		cmp	al,0x00
+		jz	short common_str_err
+		mov	dx,str_create_fail
+
+common_str_err:	mov	ah,0x09
+		int	21h
+		mov	ah,0x09
+		mov	dx,str_crlf
+		int	21h
+		ret
+
+		segment .data
+
+str_deleted:	db	'Deleted$'
+str_del_fail:	db	'Delete failed$'
+str_renamed:	db	'Renamed$'
+str_created:	db	'Created$'
+str_create_fail:db	'Create failed$'
+str_crlf:	db	13,10,'$'
+
+		segment .bss
+
+fcb:		resb	0x2A		; for creation
+fcbren:		resb	0x2A		; for renaming
+
+; WARNING: We don't know how large DOS will make the record size, therefore this must be last!
+dta:		resb	0x100
+
