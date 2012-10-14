7a445ba541e543667b2501bcb1973bf15491a207
root <root@tvbox22.(none)>
2012-10-13 18:34:58 -0700

    iii

---
+++ dos/asmexam/reset95.asm
@@ -0,0 +1,57 @@
+;--------------------------------------------------------------------------------------
+; RESET95.COM
+;
+; Use INT 21h AX=0x710D to reset a drive under MS-DOS 7/Windows 95
+;--------------------------------------------------------------------------------------
+		bits 16			; 16-bit real mode
+		org 0x100		; DOS .COM executable starts at 0x100 in memory
+
+		segment .text
+
+		push	cs
+		pop	ds
+
+		xor	cx,cx
+; we allow a 0, 1, or 2 on the command line to tell us which reset function to use
+		mov	si,0x81		; command line
+l1:		lodsb
+		cmp	al,' '
+		jz	l1
+		cmp	al,'0'
+		jb	l1e
+		cmp	al,'9'
+		jae	l1e
+		mov	cl,al
+		sub	cl,'0'
+l1e:
+; so at this point: CX = either 0 or the single-digit value read from command line
+		stc
+		mov	ax,0x710D	; Windows 95 reset drive
+		mov	dx,0		; default drive
+		int	21h
+		jnc	reset_ok	; CF=0 if OK
+		; OR.... it seems Windows 95 returns with AX=0x7100 and CF=1
+		; whatever the fuck that means...
+		cmp	ax,0x7100
+		jz	reset_ok
+
+		mov	dx,str_failed
+		call	puts
+		ret
+
+reset_ok:
+
+; EXIT to DOS
+exit:		ret
+
+;------------------------------------
+puts:		mov	ah,0x09
+		int	21h
+		ret
+
+		segment .data
+
+str_failed:	db	'Failed: ',13,10,'$'
+
+		segment .bss
+
