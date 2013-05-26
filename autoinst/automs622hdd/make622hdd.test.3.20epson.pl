5a3a21fb682b9c3f32c11df6fe9315c8bd6e867d
Jonathan Campbell <jonathan@castus.tv>
2013-05-25 21:35:11 -0700

    DOSBox-based test scripts now read geometry from partition table.

--- autoinst/automs622hdd/make622hdd.test.3.20epson.pl
+++ autoinst/automs622hdd/make622hdd.test.3.20epson.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/perl
-# MS-DOS 3.2 runs in QEMU, but does not support the 1.44MB floppy format.
-# Unfortunately QEMU does not support the 720KB floppy format it does
-# support. So we use DOSBox.
+# MS-DOS 4.01 does not run in QEMU or Bochs, though it does in VirtualBox.
+# But I don't like to monkey around with VirtualBox's storage manager,
+# so instead, we'll use DOSBox-X.
 
 # The build script produces a VDI regardless for convenience and
 # for the time when eventually QEMU fixes whatever is wrong.
 
-# FIXME: MS-DOS 3.2 relies on the C/H/S values of the drive! It's not
-#        like later versions where it doesn't seem to matter.
+my $disk = "msdos320epsonhdd";
 
 # first we need to read back the geometry.
 # being MS-DOS 4.01 it's likely it only supports 63 sectors/track 16 heads
@@ -16,8 +15,24 @@
 # geometry anyway from the partition table when we add support to the
 # builder script for geometries other than 63 sectors/track 16 heads/track.
 my $sects = 63,$heads = 16,$cyls,$sectors;
-$sectors = ( -s "../../build/msdos320epsonhdd" ) / 512;
-$cyls = $sectors / $sects / $heads;
+$sectors = ( -s "../../build/$disk" ) / 512;
+
+# read the partition to get the actual geometry
+my $xx,$a,$b,$c;
+open(X,"<","../../build/$disk") || die;
+binmode(X);
+seek(X,0x1BE+5,0);
+read(X,$xx,3);
+($a,$b,$c) = unpack("ccc",$xx);
+$heads = $a;
+$sects = $b & 0x3F;
+$cyls = (($b >> 6) << 8) + $c;
+$heads++;
+$cyls++;
+close(X);
+
+# compute
+$cyls = int($sectors / $sects / $heads);
 print "Geometry: C/H/S $cyls/$heads/$sects\n";
 
 my $fboot = 0;
@@ -28,17 +43,17 @@ while (@ARGV > 0) {
 }
 
 # generate a minimalist dosbox.conf
-open(X,">../../build/msdos320epsonhdd.dosbox.conf") || die;
+open(X,">../../build/$disk.dosbox.conf") || die;
 print X "[ide, primary]\n";
 print X "enable=true\n";
 print X "\n";
 print X "[autoexec]\n";
-print X "imgmount 2 msdos320epsonhdd -t hdd -fs none -size 512,$sects,$heads,$cyls -ide 1m\n";
+print X "imgmount 2 $disk -t hdd -fs none -size 512,$sects,$heads,$cyls -ide 1m\n";
 if ($fboot) {
-	print X "boot boot320.dsk\n";
+	print X "boot boot.dsk\n";
 }
 else {
-	print X "imgmount 0 boot320.dsk -t floppy -fs none\n";
+	print X "imgmount 0 boot.dsk -t floppy -fs none\n";
 	print X "boot -l c\n";
 }
 print X "\n";
@@ -46,5 +61,5 @@ close(X);
 
 # go into the build dir and run DOSBox
 chdir("../../build") || die;
-system("dosbox-x -conf msdos320epsonhdd.dosbox.conf");
+system("dosbox-x -conf $disk.dosbox.conf");
 
--- autoinst/automs622hdd/make622hdd.test.4.01.pl
+++ autoinst/automs622hdd/make622hdd.test.4.01.pl
@@ -6,8 +6,7 @@
 # The build script produces a VDI regardless for convenience and
 # for the time when eventually QEMU fixes whatever is wrong.
 
-# FIXME: MS-DOS 3.2 relies on the C/H/S values of the drive! It's not
-#        like later versions where it doesn't seem to matter.
+my $disk = "msdos401hdd";
 
 # first we need to read back the geometry.
 # being MS-DOS 4.01 it's likely it only supports 63 sectors/track 16 heads
@@ -16,22 +15,51 @@
 # geometry anyway from the partition table when we add support to the
 # builder script for geometries other than 63 sectors/track 16 heads/track.
 my $sects = 63,$heads = 16,$cyls,$sectors;
-$sectors = ( -s "../../build/msdos401hdd" ) / 512;
-$cyls = $sectors / $sects / $heads;
+$sectors = ( -s "../../build/$disk" ) / 512;
+
+# read the partition to get the actual geometry
+my $xx,$a,$b,$c;
+open(X,"<","../../build/$disk") || die;
+binmode(X);
+seek(X,0x1BE+5,0);
+read(X,$xx,3);
+($a,$b,$c) = unpack("ccc",$xx);
+$heads = $a;
+$sects = $b & 0x3F;
+$cyls = (($b >> 6) << 8) + $c;
+$heads++;
+$cyls++;
+close(X);
+
+# compute
+$cyls = int($sectors / $sects / $heads);
 print "Geometry: C/H/S $cyls/$heads/$sects\n";
 
+my $fboot = 0;
+
+while (@ARGV > 0) {
+	my $a = shift(@ARGV);
+	$fboot = 1 if $a eq "floppy";
+}
+
 # generate a minimalist dosbox.conf
-open(X,">../../build/msdos401hdd.dosbox.conf") || die;
+open(X,">../../build/$disk.dosbox.conf") || die;
 print X "[ide, primary]\n";
 print X "enable=true\n";
 print X "\n";
 print X "[autoexec]\n";
-print X "imgmount 2 msdos401hdd -t hdd -fs none -size 512,$sects,$heads,$cyls -ide 1m\n";
-print X "boot -l c\n";
+print X "imgmount 2 $disk -t hdd -fs none -size 512,$sects,$heads,$cyls -ide 1m\n";
+if ($fboot) {
+	print X "boot boot.dsk\n";
+}
+else {
+	print X "imgmount 0 boot.dsk -t floppy -fs none\n";
+	print X "boot -l c\n";
+}
 print X "\n";
 close(X);
 
 # go into the build dir and run DOSBox
 chdir("../../build") || die;
-system("dosbox-x -conf msdos401hdd.dosbox.conf");
+system("dosbox-x -conf $disk.dosbox.conf");
 
