469a7c144a5352b6faeaa6fcf58c5a44ee3df086
Jonathan Campbell <jonathan@castus.tv>
2013-06-01 10:06:14 -0700

    more work

--- autoinst/automs622hdd/make622hdd.everything.sh
+++ autoinst/automs622hdd/make622hdd.everything.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 2.1 3.2epson 3.3 3.3nec 4.01 5.0 6.0 6.20 6.21 6.22 7.0 7.0sp1 7.1osr2 7.1win98 7.1win98se 8.0winme; do
+for i in 2.1 2.2td 3.2epson 3.3 3.3nec 4.01 5.0 6.0 6.20 6.21 6.22 7.0 7.0sp1 7.1osr2 7.1win98 7.1win98se 8.0winme; do
 	./make622hdd.pl $* --ver $i || break
 done
 
