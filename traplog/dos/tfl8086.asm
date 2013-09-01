44914daa507fc3e5991525be42574a6cd9bfa5cc
Jonathan Campbell <jonathan@castus.tv>
2013-09-01 14:14:12 -0700

    tfl8086: it works, so far. some programs in DOS seem to kill the TF flag, but, logging works. Next up: a simple tool to decode the log.

--- traplog/dos/tfl8086.asm
+++ traplog/dos/tfl8086.asm
@@ -36,6 +36,10 @@ REC_LENGTH	EQU			36
 		push	cs
 		pop	ds
 
+		mov	[intstack_seg],cs
+		mov	word [vga_seg],0xB800		; TODO: Autodetect mono/color segment
+		mov	word [logfd],0
+
 ; read the command line, skip leading whitespace
 		mov	si,0x81
 ld1:		lodsb
@@ -43,6 +47,7 @@ ld1:		lodsb
 		jz	ld1
 		dec	si
 		mov	[exec_path],si
+		mov	word [record_buf_write],0
 
 ; and then NUL-terminate the line
 		mov	bl,[0x80]
@@ -96,6 +101,15 @@ ld4:		lodsb
 		mov	cl,4
 		shr	bx,cl		; BX = (BX + this image size + 0xF) >> 4 = number of paragraphs
 		int	21h
+
+; open the log file
+		mov	ah,0x3C		; AH=0x3C create file
+		xor	cx,cx
+		mov	dx,logfilename
+		int	21h
+		mov	word [logfd],ax
+		jc	.creat_err
+.creat_err:
 
 ; OK proceed
 		cld
@@ -173,7 +187,8 @@ ld4:		lodsb
 		mov	ds,bx
 		mov	es,bx
 
-		int	1		; *DEBUG*
+		mov	word [cs:min_seg],bx		; minimum segment
+		mov	word [cs:max_seg],0xA000	; maximum segment
 
 ; build IRET stack frame. I hope the DOS program doesn't rely on the stack contents at SS:SP!
 		pushf					; load FLAGS into AX
@@ -198,6 +213,9 @@ on_program_exit:
 		mov	ah,0x09
 		mov	dx,str_ok_exit
 		int	21h
+
+; flush remaining buffer
+		call	flush_record
 
 ; restore INT 01 vector
 		push	es
@@ -225,15 +243,143 @@ puts:		mov	ah,0x09
 		ret
 
 ; INT 0x01 TRAP HANDLER
-on_int1_trap:	push	ax
+on_int1_trap:	cli
+
+		; save the stack pointer off and switch to our own stack.
+		; we cannot make any assumptions about the stack we're on.
+		; for all we know, the program code may rely on stack data
+		; above the stack pointer as well as below. If we just "push"
+		; data onto the stack we'll obliterate that data.
+		mov	word [cs:intstack_save],sp
+		mov	word [cs:intstack_save+2],ss
+		mov	ss,word [cs:intstack_seg]
+		mov	sp,intstack_end - 2
+
+		push	ax
 		push	bx
-		mov	ah,0x0E
-		mov	al,'T'
-		xor	bx,bx
-		int	10h
+		push	cx
+		push	dx
+		push	si
+		push	di
+		push	bp
+		push	ds
+		push	es
+
+		mov	ax,cs
+		mov	ds,ax
+		mov	ax,word [cs:intstack_save+2]
+		mov	es,ax				; our ES = caller's SS
+		mov	si,word [cs:intstack_save]	; our SI = caller's SP
+
+		mov	ax,word [es:si+2]		; read segment from IRET frame
+
+		mov	bp,sp				; SS:BP = pointer to saved CPU register state on stack
+							;         ES,DS,BP,DI,SI,DX,CX,BX,AX = 18 bytes
+
+		; if the trap occured below the PSP segment, or above system memory,
+		; then do not log the CPU state. we are not interested in tracing
+		; the DOS or the BIOS. in fact, we call into DOS to write the CPU
+		; state log to disk, so NOT tracing DOS avoids reentrancy issues.
+		cmp	ax,[min_seg]
+		jb	.return
+		cmp	ax,[max_seg]
+		jae	.return
+
+		cmp	word [record_buf_write],record_buf_size - REC_LENGTH
+		jbe	.no_flush
+		call	flush_record
+.no_flush:
+
+; record CPU state
+		mov	bx,[record_buf_write]
+		lea	di,[record_buf+bx]
+		mov	word [di + cpu_state_record_8086.r_recid],REC_8086
+		mov	word [di + cpu_state_record_8086.r_reclen],REC_LENGTH
+		mov	ax,[bp+0]
+		mov	word [di + cpu_state_record_8086.r_es],ax
+		mov	ax,[bp+2]
+		mov	word [di + cpu_state_record_8086.r_ds],ax
+		mov	ax,[bp+4]
+		mov	word [di + cpu_state_record_8086.r_bp],ax
+		mov	ax,[bp+6]
+		mov	word [di + cpu_state_record_8086.r_di],ax
+		mov	ax,[bp+8]
+		mov	word [di + cpu_state_record_8086.r_si],ax
+		mov	ax,[bp+10]
+		mov	word [di + cpu_state_record_8086.r_dx],ax
+		mov	ax,[bp+12]
+		mov	word [di + cpu_state_record_8086.r_cx],ax
+		mov	ax,[bp+14]
+		mov	word [di + cpu_state_record_8086.r_bx],ax
+		mov	ax,[bp+16]
+		mov	word [di + cpu_state_record_8086.r_ax],ax
+		mov	si,word [intstack_save]			; our SI = caller's SP
+		mov	word [di + cpu_state_record_8086.r_sp],si
+		mov	ax,word [es:si]
+		mov	word [di + cpu_state_record_8086.r_ip],ax
+		mov	ax,word [es:si+2]
+		mov	word [di + cpu_state_record_8086.r_cs],ax
+		mov	ax,word [es:si+4]
+		mov	word [di + cpu_state_record_8086.r_flags],ax
+		mov	ax,word [intstack_save+2]		; caller's SS
+		mov	word [di + cpu_state_record_8086.r_ss],ax
+
+		push	ds
+		mov	bx,word [es:si]				; BX = IP
+		mov	ds,word [es:si+2]			; DS = CS
+		mov	cx,[bx]					; first 4 bytes at CS:IP
+		mov	dx,[bx+2]
+		pop	ds
+		mov	word [di + cpu_state_record_8086.r_csip_capture],cx
+		mov	word [di + cpu_state_record_8086.r_csip_capture + 2],dx
+
+; increment record
+		add	word [record_buf_write],REC_LENGTH
+
+; animate something on the VGA display to show we're still executing
+		push	es
+		mov	es,word [cs:vga_seg]
+		inc	word [es:158]
+		pop	es
+
+.return:	pop	es
+		pop	ds
+		pop	bp
+		pop	di
+		pop	si
+		pop	dx
+		pop	cx
 		pop	bx
 		pop	ax
+
+		cli
+		mov	ss,word [cs:intstack_save+2]
+		mov	sp,word [cs:intstack_save]
 		iret
+
+; write record to disk
+flush_record:	cmp	word [record_buf_write],0
+		jz	.exit				; nothing to write, return
+
+		cmp	word [logfd],0
+		jz	.exit				; no valid file descriptor, return
+
+		push	ax
+		push	bx
+		push	cx
+		push	dx
+		mov	ah,0x40
+		mov	bx,word [logfd]
+		mov	cx,word [record_buf_write]
+		mov	dx,record_buf
+		int	21h
+		pop	dx
+		pop	cx
+		pop	bx
+		pop	ax
+
+		mov	word [record_buf_write],0
+.exit		ret
 
 		segment .data
 
@@ -243,7 +389,11 @@ str_ok_exit:	db	'Exec OK. Sub-program terminated normally.',13,10,'$'
 str_need_param:	db	'Need a program to run'
 crlf:		db	13,10,'$'
 
+logfilename:	db	'TF8086.LOG',0
+
 		segment .bss
+
+logfd:		resw	1
 
 old_int01:	resd	1
 
@@ -251,10 +401,24 @@ exec_path:	resw	1
 stack_beg:	resb	0x400-1
 stack_end:	resb	1
 
+intstack_save:	resd	1
+intstack_beg:	resb	0x400-1
+intstack_end:	resb	1
+intstack_seg:	resw	1
+
 exec_fcb:	resb	24
 exec_pblk:	resb	0x14
 
 exec_cmdtail:	resb	130
+
+min_seg:	resw	1		; minimum segment to trace
+max_seg:	resw	1		; maximum segment to trace
+vga_seg:	resw	1
+
+record_buf_write:resw	1
+record_buf:	resb	record_buf_size
+record_buf_end	equ	$
+record_buf_size equ	4096
 
 ENDOFIMAGE:	resb	1		; this offset is used by the program to know how large it is
 
