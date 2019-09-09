fefccd97550bfb4b5763f168f272c2f5e48cb088
Jonathan Campbell <jonathan@castus.tv>
2019-09-08 22:48:29 -0700

    begin

---
+++ dos/asmexam/getvser.asm
@@ -0,0 +1,61 @@
+;--------------------------------------------------------------------------------------
+; GETVSER.COM
+;
+; Read the label and serial number from the current disk 
+;
+; Known issues:
+;--------------------------------------------------------------------------------------
+		bits 16			; 16-bit real mode
+		org 0x100		; DOS .COM executable starts at 0x100 in memory
+
+		segment .text
+
+		push	cs
+		pop	ds
+		push	cs
+		pop	es
+
+; zero memory in FCB
+		cld
+		mov	di,info
+		xor	ax,ax
+		mov	cx,0x20/2
+		rep	stosw
+
+; get the volume serial and other info
+        mov ax,0x6900   ; get serial number (AH=69h AL=00h)
+        xor bx,bx       ; bl = 0 (default drive)  bh = info level 0
+        mov dx,info     ; DS:DX = disk info
+        int 21h
+        jnc print_info
+
+        mov ah,0x09
+        mov dx,no_info_str
+        int 21h
+        jmp done
+
+; print what was returned
+print_info:
+        mov ah,0x09
+        mov dx,vol_is
+        int 21h
+
+        ; TODO
+
+		mov	ah,0x09
+		mov	dx,crlf
+		int	21h
+
+done:		xor	ah,ah
+		int	21h
+
+		segment .data
+
+no_info_str:db  'Unable to get info',13,10,'$'
+vol_is:     db  'Volume serial is: $'
+crlf:		db	13,10,'$'
+
+		segment .bss
+
+info:       resb    0x20
+
